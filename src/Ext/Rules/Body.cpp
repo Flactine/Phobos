7ccde6d4602af2634b2419235a35b811d2694071
Flactine <1716455702@qq.com>
2026-09-10 00:35:33 +0800

    半透明血条
    https://github.com/Phobos-developers/Phobos/pull/2040/changes

--- src/Ext/Rules/Body.cpp
+++ src/Ext/Rules/Body.cpp
@@ -636,6 +636,8 @@ void RulesExt::ExtData::LoadBeforeTypeData(RulesClass* pThis, CCINIClass* pINI)
 
 	this->SortCameoByName.Read(exINI, GameStrings::General, "SortCameoByName");
 
+	this->ExtendedHealthBarDrawing.Read(exINI, GameStrings::AudioVisual, "ExtendedHealthBarDrawing");
+
 	this->MergeBuildingDamage.Read(exINI, GameStrings::CombatDamage, "MergeBuildingDamage");
 
 	this->ApplyPerTargetEffectsOnDetonate.Read(exINI, GameStrings::CombatDamage, "ApplyPerTargetEffectsOnDetonate");
@@ -1348,6 +1350,7 @@ void RulesExt::ExtData::Serialize(T& Stm)
 		.Process(this->Interceptor_GuardRange_IsCylindrical)
 		.Process(this->Interceptor_ApplyFirepowerMult)
 		.Process(this->SortCameoByName)
+		.Process(this->ExtendedHealthBarDrawing)
 		.Process(this->MergeBuildingDamage)
 		.Process(this->BuildingRadioLink_SyncOwner)
 		.Process(this->ApplyPerTargetEffectsOnDetonate)
--- src/Ext/Rules/Body.h
+++ src/Ext/Rules/Body.h
@@ -564,6 +564,8 @@ public:
 
 		Valueable<bool> SortCameoByName;
 
+		Valueable<bool> ExtendedHealthBarDrawing;
+
 		Valueable<bool> BuildingRadioLink_SyncOwner;
 
 		Valueable<Leptons> ExtraRange_TargetMoving;
@@ -1236,6 +1238,8 @@ public:
 			, Interceptor_ApplyFirepowerMult { true }
 
 			, SortCameoByName { false }
+
+			, ExtendedHealthBarDrawing { false }
 
 			, MergeBuildingDamage { false }
 
